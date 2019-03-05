61b28d526d19a6c60341e0954ba3d6d8577fbf52
Pierre Ramet <pierre.ramet@inria.fr>
2019-03-05 11:11:53 +0100

    start

---
+++ bib/auto/EDF.el
@@ -0,0 +1,20 @@
+(TeX-add-style-hook
+ "EDF"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "t:LaBRI::SM15"
+    "moustafa:hal-01078362"
+    "C:LaBRI::sweep2013"
+    "c:LaBRI::sweep-solhar"
+    "c:LaBRI::sweep-scheduling"
+    "A:LaBRI::ane2014"
+    "C:LaBRI::neutron2013"
+    "A:LaBRI::jcp2011"
+    "C:LaBRI::neutron2009"
+    "C:LaBRI::neutron2008"
+    "C:LaBRI::ddm2008"
+    "C:LaBRI::PMAA2008a"
+    "t:LaBRI::BL10"
+    "faverge:hal-01630208"))
+ :bibtex)
+
